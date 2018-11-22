47b20784e32902d04ef175475b94d2803ff5ee04
Mert Ozkarakoc <mert.ozkarakoc@gmail.com>
2018-11-22 11:37:59 +0300

    regression tests

---
+++ test/regressions/tests/DocCode/DocCode.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { DocCode as BDocCode } from '@boa/components/DocCode';
+
+export default function DocCode() {
+  return (
+    <BDocCode content="console.log('Hello world');" lang="js" />
+  );
+}
